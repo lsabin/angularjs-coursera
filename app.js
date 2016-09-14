185dafd7263c288c4cca23b07b83e2dab16dfccc
Leandro Sabín <leandro.sabin@qubitia.com>
2016-09-14 20:02:39 +0200

    colores

--- app.js
+++ app.js
@@ -9,19 +9,24 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.message = "";
   $scope.lunchList = "";
+  $scope.messageStyle = "";
 
 
   $scope.checkList = function () {
     if ($scope.lunchList.length == 0) {
       $scope.message = "Please enter data first";
+      $scope.messageStyle = "";
     } else {
       var arrayOfFood = $scope.lunchList.split(",");
       if (arrayOfFood.length == 0) {
         $scope.message = "Please enter data first";
+        $scope.messageStyle = "";
       } else if (arrayOfFood.length <= 3) {
         $scope.message = "Enjoy!";
+        $scope.messageStyle = "green";
       } else if (arrayOfFood.length > 3) {
         $scope.message = "Too much!";
+        $scope.messageStyle = "red";
       }
 
     }
